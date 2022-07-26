32c057136977b2fc61d00fbd2cce78a7ce989d92
Sokol8807 <sokol8807@gmail.com>
2022-07-26 22:02:15 +0300

    Initial Commit

---
+++ TwoSquareStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoSquareStoryboard
+//
+//  Created by Ilya Sokolov on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
